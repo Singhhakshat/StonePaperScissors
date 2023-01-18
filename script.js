89e21ba3ca2db7a3833916e50bbe68dbdf01a212
Singhhakshat <50708393+Singhhakshat@users.noreply.github.com>
2023-01-19 02:45:03 +0530

    Restructured Repo

--- script.js
+++ script.js
@@ -1,169 +1,210 @@
 const element={
-    startButton: document.getElementById("startButton"),
-
-    roundNumber: document.getElementById("round"),
-
-    stonePicture:document.getElementById("rockImage"),
-    paperPicture:document.getElementById("paperImage"),
-    scissorsPicture:document.getElementById("scissorsImage"),
+    horizontalRule: document.getElementsByClassName('horizontalRule'),
+    gameTitle: document.getElementById("title"),
+    gameContainer: document.getElementById("gameContainer"),
+    startButton: document.getElementById("startGame"),
+    round: document.getElementById("round"),
+    playerSelect: document.getElementById("playerSelect"), 
+    rockImage: document.getElementById("rockImage"),
+    paperImage: document.getElementById("paperImage"),
+    scissorsImage: document.getElementById("scissorsImage"),
+    nextRoundButton: document.getElementById("nextRound"),
+    showCurrent: document.getElementById("showCurrent"),
+    playerChoice: document.getElementById("playerChoice"),
+    computerChoice: document.getElementById("computerChoice"),
+    result:document.getElementById("result"),
     gameScore: document.getElementById("gameScore"),
-    score:{
-        round1:document.getElementById("roundOneImage"),
-        round2:document.getElementById("roundTwoImage"),
-        round3:document.getElementById("roundThreeImage")
-    },
-    show:{
-        show: document.getElementById('show'),
-        playerChoice: document.getElementById('playerChoice'),
-        computerChoice: document.getElementById('computerChoice'),
-        status: document.getElementById('status')
-    },
-    finalMessage: document.getElementById("finalMessage"),
-    finalScore: document.getElementById("finalScore")
-};
-const score={
-    player: 0,
-    computer: 0
+    playerScore: document.getElementById("playerScore"),
+    computerScore: document.getElementById("computerScore"),
+    endGame: document.getElementById("endGame"),
+    finalScorePlayer: document.getElementById("finalScorePlayer"),
+    finalScoreComputer: document.getElementById("finalScoreComputer"),
+    finalScoreTies: document.getElementById("finalScoreTies"),
+    finalText:document.getElementById("finalText"),
+    finalImage: document.getElementById("finalImage")
 }
 
-// let player='na';
+//1 means rock, 2 means paper, 3 means scissors and 4 means tie
 
-function startAnimation(){
-
-    element.startButton.classList.add('StartGameAnimation');
-    element.startButton.style.height="100vh";
-    element.startButton.style.width="100vw";
-
-    document.getElementById("startText").style.display="none";
-    
-    setTimeout(function(){
-        element.startButton.classList.remove('StartGameAnimation');
-        document.getElementById("gameContainer").style.display="block";
-    }, 1000);
-    game();
+const gameStats={
+    computerSelection: 0,
+    playerSelection: 0,
+    enable: false,
+    round: 1,
+    playerScore: 0,
+    computerScore: 0,
+    ties:0,
+    nextButton: false,
 }
 
 function computerSelection(){
-    let choices=["Rock", "Paper", "Scissors"];
-    return choices[Math.floor(Math.random()*(2-0+1)+0)];
+    gameStats.computerSelection= Math.floor(Math.random() * 3) + 1;
 }
 
-function calcResult(computerSelection,playerSelection){
-    let result;
-    if(playerSelection==computerSelection){
-        result=2;
+function updateRound(){
+    element.playerScore.innerHTML=`You- ${gameStats.playerScore}`;
+    element.computerScore.innerHTML=`Computer- ${gameStats.computerScore}`;
+}
+
+function startAnimation(){
+    element.gameContainer.classList.add('StartGameAnimation');
+    element.gameContainer.style.height="100vh";
+    element.gameContainer.style.width="100vw";
+
+    element.startButton.style.display="none";   
+    element.gameTitle.style.display="none";
+
+    element.round.style.display="block";
+    element.playerSelect.style.display="block";
+    element.nextRoundButton.style.display="block";
+    element.gameScore.style.display="flex";
+    element.horizontalRule[0].style.display="block";
+    element.horizontalRule[1].style.display="block";
+}
+
+function startGame(){
+    startAnimation();
+    updateRound();
+    nextButton(false);
+    element.round.innerHTML=`Round ${gameStats.round}`;
+    gameStats.enable=true;
+}
+
+function playRound(value){
+    nextButton(false);
+    if(gameStats.enable){
+        gameStats.playerSelection=value;
+        computerSelection();
+        calcResult();
+        updateRound();
+    }
+}
+
+//for result, 0 means tie, 1 means won, 2 means lost
+function calcResult(){
+    gameStats.enable=false;
+    if(gameStats.playerSelection==1){
+        if(gameStats.computerSelection==1){
+            showGame(0);
+            gameStats.ties++;
+        }
+        if(gameStats.computerSelection==2){
+            gameStats.computerScore++;
+            showGame(2);
+        }
+        if(gameStats.computerSelection==3){
+            gameStats.playerScore++;
+            showGame(1);
+        }
+    }
+    if(gameStats.playerSelection==2){
+        if(gameStats.computerSelection==2){
+            showGame(0);
+            gameStats.ties++;
+        }
+        if(gameStats.computerSelection==3){
+            gameStats.computerScore++;
+            showGame(2);
+        }
+        if(gameStats.computerSelection==1){
+            gameStats.playerScore++;
+            showGame(1);
+        }
+    }
+    if(gameStats.playerSelection==3){
+        if(gameStats.computerSelection==3){
+            showGame(0);
+            gameStats.ties++;
+        }
+        if(gameStats.computerSelection==1){
+            gameStats.computerScore++;
+            showGame(2);
+        }
+        if(gameStats.computerSelection==2){
+            gameStats.playerScore++;
+            showGame(1);
+        }
+    }
+}
+
+function showGame(value){
+    element.playerSelect.style.display='none';
+    element.showCurrent.style.display='block';
+
+    switch (gameStats.playerSelection) {
+        case 1:
+            element.playerChoice.src='media/rock.jpeg';
+            break;
+        case 2:
+            element.playerChoice.src='media/paper.jpeg';
+            break;
+        case 3:
+            element.playerChoice.src='media/scissors.jpeg';
+    }
+    switch (gameStats.computerSelection) {
+        case 1:
+            element.computerChoice.src='media/rock.jpeg';
+            break;
+        case 2:
+            element.computerChoice.src='media/paper.jpeg';
+            break;
+        case 3:
+            element.computerChoice.src='media/scissors.jpeg';
+    }
+    if(value==1)
+        element.result.innerHTML='You Won';
+    else if(value==2)
+        element.result.innerHTML='Computer Won';
+    else{
+        element.result.innerHTML='Its a Tie';
+        gameStats.round--;
+    }
+    nextButton(true);
+    if(value==0){
+        element.nextRoundButton.innerHTML='Retry';
+    }
+}
+
+function nextButton(value,val1){
+    element.nextRoundButton.innerHTML='Next Round';
+    if(value==true)
+        element.nextRoundButton.style.backgroundColor='rgb(26,135,13)';
+    else
+        element.nextRoundButton.style.backgroundColor='gray';
+    
+    gameStats.nextButton=val1;
+}
+
+function nextRound(){
+    nextButton(false,true);
+    if(gameStats.nextButton && gameStats.round<3){
+        gameStats.round++;
+        element.round.innerHTML=`Round ${gameStats.round}`;
+
+        gameStats.enable=true;
+        element.showCurrent.style.display='none';
+        element.playerSelect.style.display='block';
     }
     else{
-        if(playerSelection=="Rock"){
-            if(computerSelection=="Scissors")
-                result=1;
-            if(computerSelection=="Paper")
-                result=0;
-        }
-        if(playerSelection=="Paper"){
-            if(computerSelection=="Scissors")
-                result=0;
-            if(computerSelection=="Rock")
-                result=1;
-        }
-        if(playerSelection=="Scissors"){
-            if(computerSelection=="Rock")
-                result=0;
-            if(computerSelection=="Paper")
-                result=1;
-        }
-        return result;
+        element.nextRoundButton.innerHTML='Show Result'
+        result();
     }
 }
 
-function showScore(round,result, computer, player){
+function result(){
+    element.gameContainer.style.display='none';
+    element.endGame.style.display='flex';
 
-    element.show.playerChoice.innerHTML=`Player Choice: ${player}`;
-    element.show.computerChoice.innerHTML=`Computer Choice: ${computer}`;
-    if(result==1)
-        element.show.status.innerHTML='You Won!';
-    else if(result==0)
-        element.show.status.innerHTML='Computer Won!';
-    else
-        element.show.status.innerHTML=`Its a tie -> Play again`;
-
-    if(round==1)
-        element.score['round1'].src=(result==1)?'media/won.png':'media/lost.png';
-    if(round==2)
-        element.score['round2'].src=(result==1)?'media/won.png':'media/lost.png';
-    if(round==3)
-        element.score['round3'].src=(result==1)?'media/won.png':'media/lost.png';
-}
-
-function finalResult(){
-    if(score.player>=2)
-        return true;
-    else
-        return false;
-}
-
-function endofGame(){
-    let result=finalResult();
-    element.finalMessage.innerHTML=result?'You won':'Computer Won';
-    element.finalScore.style.backgroundColor=result?'lightgreen':'red';
-    element.show.show.style.display='none';
-    element.finalScore.style.display='block';
-}
-
-// function wait() {
-//     if (player==='na') {
-//         setTimeout(wait, 1000);
-//     } else {
-//       console.log(`playerSelection: ${player}`);
-//     }
-//   }
-
-// function makePlayerSelection(newValue){
-//     player=newValue;
-// }
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-async function getVal(){
-    await sleep(3000)
-     return prompt("Enter your choise: \"Rock\"/ \"Paper\"/\"Scissors\"");
-   
+    element.finalScorePlayer.innerHTML=`Player Score: ${gameStats.playerScore}`;
+    element.finalScoreComputer.innerHTML=`Computer Score: ${gameStats.computerScore}`;
+    element.finalScoreTies.innerHTML=`Ties: ${gameStats.ties}`;
     
-}
-
-async function game(){
-    let i=1;
-    while(i <=3){
-        // makePlayerSelection('na');
-        element.roundNumber.innerHTML=`Round ${i}`;    //showing round number on top.
-        let computer=computerSelection();
-        console.log(computer);
-        
-        await getVal().then(
-            function(value){
-               let result = calcResult(computer, value);
-                console.log(result);
-                if(result==1)
-                    score.player++;
-                else if(result==0)
-                    score.computer++;
-                else{
-                    console.log("Its a Tie!");
-                    i--;
-                }
-                console.log(score);
-                showScore(i,result, computer, value);
-                console.log('end of game function')
-            },
-             function(value){
-              
-             }
-          
-        )
-        
-        i++;
+    if(gameStats.playerScore>gameStats.computerScore){
+        element.finalText.innerHTML='You Won';
+        element.finalImage.src='media/happy.jpeg';
     }
-
-    setTimeout(function(){
-        endofGame();
-    }, 500);
+    else{
+        element.finalText.innerHTML='Computer Won';
+        element.finalImage.src='media/sad.jpg';
+    }
 }
